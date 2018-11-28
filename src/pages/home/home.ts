eec35408247faa53ca49cb11737b3b95375ee732
teeuguima <mateeusguimaraes10@gmail.com>
2018-11-27 22:31:15 -0300

    initial commit

---
+++ src/pages/home/home.ts
@@ -0,0 +1,15 @@
+import { LoginPage } from './../login/login';
+import { Component } from '@angular/core';
+import { NavController } from 'ionic-angular';
+
+@Component({
+  selector: 'page-home',
+  templateUrl: 'home.html'
+})
+export class HomePage {
+
+  constructor(public navCtrl: NavController) {
+      this.navCtrl.setRoot(LoginPage);
+  }
+
+}
