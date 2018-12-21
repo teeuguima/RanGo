292a61f09b3cad066462e15650f1da02160ef114
unknown <lucasmlevi@gmail.com>
2018-12-21 08:45:33 -0300

    Integração do cadastro com o servidor.

--- src/pages/cadastro2/cadastro2.ts
+++ src/pages/cadastro2/cadastro2.ts
@@ -2,7 +2,7 @@ import { CadastroInfo } from './../../app/app.module';
 import { HomePage } from './../home/home';
 
 import { Component } from '@angular/core';
-import { IonicPage, NavController, NavParams } from 'ionic-angular';
+import { IonicPage, NavController, NavParams, ToastController } from 'ionic-angular';
 import {Socket} from 'ng-socket-io';
 import { LoginPage } from '../login/login';
 
@@ -28,11 +28,21 @@ export class Cadastro2Page {
   formPagament: string;
   sexo: string;
 
-  constructor(public navCtrl: NavController, public navParams: NavParams, public socket: Socket ) {
+  constructor(public navCtrl: NavController, public navParams: NavParams, public socket: Socket, private toastCtrl: ToastController ) {
+    socket.on('retorno-cadastro-vendedor', (retorno) => {
+      if (retorno === 0) { // sucesso
+        this.showToast('Cadastro realizado com sucesso.');
+        navCtrl.push(LoginPage);
+      } else if (retorno === 1) { // cpf repetido
+        this.showToast('Cadastro não realizado. CPF já cadastrado no sistema.');
+      } else if (retorno === 2) { // email repetido
+        this.showToast('Cadastro não realizado. Email já cadastrado no sistema.');
+      }
+    });
   }
 
   nextPage(){
-    this.navCtrl.push(LoginPage);
+    this.cadastroResponsavel();
   }
 
 
@@ -42,20 +52,27 @@ export class Cadastro2Page {
   }
 
   cadastroResponsavel(){
-    this.socket.emit('cadastro-vendedor',{
-      nome_responsavel: this.nomeResponsavel,
-      sobrenome_responsavel: this.sobrenome,
-      nome_negocio: this.navParams.get('nome'),
-      endereco_negocio: this.navParams.get('endereco'),
-      telefone_responsavel: this.navParams.get('telefone'),
-      categoria_negocio: this.navParams.get('categoria'),
-      limite_venda: this.navParams.get('limite'),
-      valor_frete: this.navParams.get('frete'),
-      cpf_responsavel: this.navParams.get('cpf'),
-      pagamento: this.formPagament,
+    this.socket.emit('cadastro-vendedor', {
+      nomeNegocio: this.navParams.get('nome'),
+      cpf: this.navParams.get('cpf'),
+      telefone: this.navParams.get('telefone'),
+      categoriaEstabelecimento: this.navParams.get('categoria'),
+      limiteEntrega: this.navParams.get('limite'),
+      valorFrete: this.navParams.get('frete'),
+      nomeVendedor: this.nomeResponsavel,
+      sobrenomeVendedor: this.sobrenome,
+      sexo: 'M',
       email: this.email,
-      senha: this.senha
-    })
+      senha: this.senha,
+      formaPagamento: this.formPagament,
+    });
+  }
+
+  showToast(msg) {
+    this.toastCtrl.create({
+      message: msg,
+      duration: 3000
+    }).present();
   }
 
 }
